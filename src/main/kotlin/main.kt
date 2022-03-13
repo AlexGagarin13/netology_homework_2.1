6e755c8e2aa3bf3162020dfc81d1403a09db82c2
Aleksey Anokhin <anoxin-aleksey@bk.ru>
2022-03-13 21:34:36 +0300

    Refactoring №1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,7 @@
 fun main() {
-    while (true) {
-        print("Введите количество лайков: ")
-        val likes = readLine()?.toUInt()
-        if (likes != null) if (likes % 10U == 1U && likes % 100U != 11U)
-            print("Понравилось $likes человеку\n")
-        else print("Понравилось $likes людям\n")
-    }
+    print("Введите количество лайков: ")
+    val likes = readLine()?.toUInt() ?: return
+    if (likes % 10U == 1U && likes % 100U != 11U)
+        print("Понравилось $likes человеку\n")
+    else print("Понравилось $likes людям\n")
 }
